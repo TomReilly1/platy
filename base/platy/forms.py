25d7a6439c0645392e20a97f65809e0c24372d14
TomReilly1 <reilly.thomas.c@gmail.com>
2022-09-08 11:27:17 -0400

    add char hiding to signup password fields

--- base/platy/forms.py
+++ base/platy/forms.py
@@ -6,8 +6,8 @@ from django.contrib.auth.models import User
 class SignUpForm(UserCreationForm):
     username = forms.CharField(max_length=30, help_text='Enter a username with a max length of 20 characters. Letters, digits and @/./+/-/_ only.')
     email = forms.EmailField(max_length=50, help_text='Enter a valid email address.')
-    password1 = forms.CharField(label='Password', help_text='Your password must contain at least 8 characters and cannot be entirely numeric. Your password should not be similar to personal information or be a commonly used password.')
-    password2 = forms.CharField(label='Confirm Password', help_text='Enter the same password as above for verification.')
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput, help_text='Your password must contain at least 8 characters and cannot be entirely numeric. Your password should not be similar to personal information or be a commonly used password.')
+    password2 = forms.CharField(label='Confirm Password', widget=forms.PasswordInput, help_text='Enter the same password as above for verification.')
 
     class Meta:
         model = User
