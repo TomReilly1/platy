b8f2fe716f0db319ee2119455d2b25c60b24f2c8
TomReilly1 <reilly.thomas.c@gmail.com>
2022-09-09 13:15:29 -0400

    added functionality to send friend requests

--- base/platy/urls.py
+++ base/platy/urls.py
@@ -6,6 +6,8 @@ urlpatterns = [
     path('signup/', views.signup_view, name='signup'),
     path('home/', views.home_view, name='home'),
     path('home/search/', views.search_view, name='search'),
+    path('home/add-friend/<str:sender>/<str:receiver>/', views.add_friend_view, name='add_friend'),
+    path('error/', views.error_view, name='error'),
     path('', views.landing_view, name='landing'),
 ]
 
--- base/platy/views.py
+++ base/platy/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.http import require_http_methods, require_GET, require_POST
+from django.db.models import Q
 from django.contrib.auth.models import User
 # from django.contrib.postgres import search
 
+from platy.models import Friends
 from platy.forms import SearchForm, SignUpForm
 
 
@@ -17,7 +19,38 @@ def landing_view(request):
 @login_required(login_url='/accounts/login/')
 @require_GET
 def home_view(request):
-    return render(request, 'home.html')
+    req_pending = Friends.objects.filter(req_sender_id=request.user.id).filter(is_accepted=False)
+    req_received = Friends.objects.filter(req_receiver_id=request.user.id).filter(is_accepted=False)
+    friends = Friends.objects.filter(is_accepted=True)\
+        .filter(Q(req_sender_id=request.user.id) | Q(req_receiver_id=request.user.id))
+    print(request.user.id)
+    print(req_pending)
+    # print(req_pending[0].req_receiver.username)
+    # print(req_received[0].req_sender.username)
+    print(friends)
+
+
+    pending_req_list = list(map(lambda obj: obj.req_receiver.username, req_pending))
+    print(pending_req_list)
+    received_req_list = list(map(lambda obj: obj.req_sender.username, req_received))
+    print(received_req_list)
+    friends_list = []
+    for f in friends:
+        if f.req_receiver.username == request.user:
+            friends_list.append(f.req_receiver.username)
+        elif f.req_sender.username == request.user:
+            friends_list.append(f.req_receiver.username)
+        else:
+            raise 'CUSTOM ERROR: Unkown error in creating friends list'
+    # friends_list = list(map(lambda obj: obj.username, friends))
+    print(friends_list)
+
+
+    return render(request, 'home.html', {
+        'friends': friends_list,
+        'pending': pending_req_list,
+        'received': received_req_list
+        })
 
 @require_http_methods(['GET', 'POST'])
 def signup_view(request):
@@ -39,9 +72,28 @@ def search_view(request):
         
         if form.is_valid():            
             search_str = form.cleaned_data.get('search_query')
-            search_request = User.objects.filter(username__startswith=search_str)[:5]
-            
-            user_list = list(map(lambda obj: {'name':obj.username}, search_request))
+            search_request = User.objects.filter(username__istartswith=search_str)[:5]
+            friends = Friends.objects.all()
+
+                        
+            user_list = []
+
+            for obj in search_request:
+                user_obj = {'name': obj.username, 'has_requested': False, 'are_friends': False}
+                print(user_obj)
+                for pair in friends:
+                    if request.user.id == pair.req_sender_id and obj.id == pair.req_receiver_id:
+                        user_obj['has_requested'] = True
+                        
+                        if pair.is_accepted:
+                            user_obj['are_friends'] = True
+                        
+                        break
+
+                user_list.append(user_obj)                        
+                print(user_list)
+            # user_list = list(map(lambda obj: {'name':obj.username}, search_request))
+
 
             return render(request, 'search.html', {'form': form, 'userlist': user_list})
     # GET Method
@@ -50,4 +102,34 @@ def search_view(request):
         return render(request, 'search.html', {'form': form, 'userlist': [{'name':''}]})
 
     return render(request, 'search.html', {'form': form})
+
+
+@login_required(login_url='/accounts/login/')
+@require_POST
+def add_friend_view(request, sender, receiver):
+    try:
+        print(sender)
+        print(receiver)
+
+        sender_user = User.objects.get(username=sender)
+        receiver_user = User.objects.get(username=receiver)
+
+        print('got users')
+        
+        print(sender_user)
+        print(receiver_user)
+        
+        friend_request = Friends(req_sender=sender_user, req_receiver=receiver_user, is_accepted=False)
+        friend_request.save()
+
+        print('made it')
+
+        return render(request, 'add_friend.html', {'target': receiver})
+    except:
+        return redirect('error')
+
+
+@require_GET
+def error_view(request):
+    return render(request, 'error.html')
 
