d915631b4c6d289a202ac2504b9b1ccc62041287
TomReilly1 <reilly.thomas.c@gmail.com>
2022-09-17 12:23:38 -0400

    place DJANGO_SETTINGS_MODULE higher for daphne to run properly

--- base/base/asgi.py
+++ base/base/asgi.py
@@ -6,19 +6,20 @@ It exposes the ASGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 """
-
 import os
-
 from django.core.asgi import get_asgi_application
+
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'base.settings')
+django_asgi_app = get_asgi_application()
+
+
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 import platy.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'base.settings')
-
-# application = get_asgi_application()
 application = ProtocolTypeRouter({
-    'http': get_asgi_application(),
+    'http': django_asgi_app,
     'websocket': AuthMiddlewareStack(
         URLRouter(
             platy.routing.websocket_urlpatterns
